82651a889ee4e7fe0b53b55fa8cbd4942ebfadcf
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-05-15 23:37:56 -0500

    Make constants private

--- src/freshdesk/store.py
+++ src/freshdesk/store.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from datetime import datetime
 
-from freshdesk.constants import ENCODING
+from freshdesk._constants import ENCODING
 
 
 @dataclass
